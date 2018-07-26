3bcab8d3cc5dce41ec6935bed7cad92e47e2ff19
Christoph Dorn <christoph@christophdorn.com>
2018-07-25 22:26:34 -0700

    wip

--- tests/07-Inheritance/main.sh
+++ tests/07-Inheritance/main.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash.origin.script
 
-# Instead of using an 'inf.json' file we can also pass JSON to the 'inf' command.
-# Passing JSON to commands is easy in the test file here since it is run using
-# bash.origin.test which adds JSON sytnax support to bash using bash.origin.module.
-
-# NOTE: When using this approach, keys may not be repeated as bash.origin.modules uses
-#       a standard JSON parser to process the JSON syntax.
-
 inf {
     "#": "../06-MapAliasToComponent",
 
